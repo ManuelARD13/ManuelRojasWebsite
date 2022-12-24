622a3bad05d3dfcb63959e9b28435f93d89c5ebd
ManuelARD13 <115947544+ManuelARD13@users.noreply.github.com>
2022-12-23 21:44:23 -0300

    Uploading the repo to github

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function LandingPage(props) {
+    return (
+    <section className="projectsSection">
+        <p> Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        </p>
+    </section>
+    )
+}
+
+export { LandingPage }
